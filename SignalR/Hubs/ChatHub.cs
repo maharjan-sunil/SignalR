9ca14b0e0e0a6979b777c9e7c2b8403ce97f86f9
sunil <sunil@codebee.dk>
2021-01-29 11:34:08 +0545

    SGR-1 initial commit

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Controllers.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void letsChat(string Cl_Name, string Cl_Message)
+        {
+            Clients.All.NewMessage(Cl_Name, Cl_Message);
+        }
+    }
+}
---
+++ SignalR/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalR.Startup))]
+namespace SignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+            //The assembly attribute adds the class to Owin's startup process, which executes the Configuration method when Owin starts up.
+            //This in turn calls the MapSignalR method, which creates routes for all SignalR hubs in the application.
+        }
+    }
+}
